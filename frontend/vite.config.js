860ab645f8161925289125c141d5e1c6e8625c5b
huangjie2599 <huangjie2599@gmail.com>
2025-07-29 05:47:11 +0000

    fix: Add capacitor http plugin to vite external deps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  // 【核心修正】: 将 base 设置为相对路径 './'
-  // 这确保了 index.html 中引用的资源路径是 ./assets/index-xxx.js 的形式，
-  // 而不是 /assets/index-xxx.js，从而解决了在App内无法找到文件的问题。
-  base: './', 
+  build: {
+    rollupOptions: {
+      external: [
+        // 忽略 Capacitor 和 Cordova 插件的打包
+        '@capacitor/core',
+        '@capacitor/splash-screen',
+        '@capacitor/status-bar',
+        '@capacitor-community/http' // 【核心修正】: 将 http 插件添加到这里
+      ]
+    }
+  }
 })

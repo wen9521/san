b8b9680fb9434a8e1952f7c1dad196b45bc863fd
huangjie2599 <huangjie2599@gmail.com>
2025-08-01 12:28:11 +0000

    修复完善

--- frontend/src/games/thirteen/utils/thirteenLogic.js
+++ frontend/src/games/thirteen/utils/thirteenLogic.js
@@ -15,7 +15,7 @@ export const handTypes = {
 };
 
 const rankValues = {
-  '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14,
+  '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14,
 };
 
 export const getRankValue = (card) => rankValues[card.rank];
